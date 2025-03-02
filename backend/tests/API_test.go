3efc4e41f672d0f74607016ba1a1080c02aba60f
bogumil.stoma <bogumil.stoma@gmail.com>
2025-03-02 19:42:55 +0100

    Test API function DeleteWord

--- backend/tests/API_test.go
+++ backend/tests/API_test.go
@@ -21,6 +21,21 @@ func init() {
 	}
 }
 
+func setupTestMutation(t *testing.T) (*gorm.DB, graph.MutationResolver) {
+	db := setupTestDB() // Assuming setupTestDB is a function that sets up your DB connection.
+	r := (&graph.Resolver{DB: db}).Mutation()
+
+	t.Cleanup(func() {
+		// Cleanup function that runs at the end of each test.
+		err := clearTestTables(db) // Your cleanup function.
+		if err != nil {
+			t.Fatalf("Failed to clear test tables: %v", err)
+		}
+	})
+
+	return db, r
+}
+
 func setupTestDB() *gorm.DB {
 	db := database.Connect()
 	return db
@@ -38,8 +53,7 @@ func clearTestTables(db *gorm.DB) error {
 }
 
 func TestAddTranslation(t *testing.T) {
-	db := setupTestDB()
-	r := (&graph.Resolver{DB: db}).Mutation()
+	db, r := setupTestMutation(t)
 
 	englishWord := "hello"
 	polishWord := "cześć"
@@ -54,15 +68,10 @@ func TestAddTranslation(t *testing.T) {
 	db.Model(&model.Translation{}).Where("word_id = ? AND translation_id = ?", translation.WordID, translation.TranslationID).Count(&count)
 
 	assert.Equal(t, int64(1), count, "Translation should be stored in the database")
-	err = clearTestTables(db)
-	if err != nil {
-		return
-	}
 }
 
 func TestAddTranslation_DuplicateEntry(t *testing.T) {
-	db := setupTestDB()
-	r := (&graph.Resolver{DB: db}).Mutation()
+	_, r := setupTestMutation(t)
 
 	englishWord := "hello"
 	polishWord := "cześć"
@@ -73,15 +82,10 @@ func TestAddTranslation_DuplicateEntry(t *testing.T) {
 	_, err = r.AddTranslation(context.Background(), englishWord, polishWord)
 	assert.Error(t, err, "Expected an error for duplicate translation")
 	assert.Contains(t, err.Error(), "translation already exists")
-	err = clearTestTables(db)
-	if err != nil {
-		return
-	}
 }
 
 func TestAddTranslation_Concurrent(t *testing.T) {
-	db := setupTestDB()
-	r := (&graph.Resolver{DB: db}).Mutation()
+	db, r := setupTestMutation(t)
 
 	englishWord := "hello"
 	polishWord := "cześć"
@@ -100,16 +104,10 @@ func TestAddTranslation_Concurrent(t *testing.T) {
 	}
 	db.Find(&model.Translation{}).Count(&count)
 	assert.Equal(t, int64(1), count, "Translation should be stored in the database")
-
-	err := clearTestTables(db)
-	if err != nil {
-		return
-	}
 }
 
 func TestAddTranslation_ConcurrencyWithDuplicate(t *testing.T) {
-	db := setupTestDB()
-	r := (&graph.Resolver{DB: db}).Mutation()
+	_, r := setupTestMutation(t)
 
 	englishWord := "hello"
 	polishWord := "cześć"
@@ -129,15 +127,10 @@ func TestAddTranslation_ConcurrencyWithDuplicate(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		<-done
 	}
-	err = clearTestTables(db)
-	if err != nil {
-		return
-	}
 }
 
 func TestAddWord_NewWord(t *testing.T) {
-	db := setupTestDB()
-	r := (&graph.Resolver{DB: db}).Mutation()
+	_, r := setupTestMutation(t)
 
 	word := "hello"
 	language := "EN"
@@ -150,15 +143,10 @@ func TestAddWord_NewWord(t *testing.T) {
 	assert.Equal(t, word, addedWord.Word, "The word should be correctly added")
 	assert.Equal(t, language, addedWord.Language, "The language should be correctly added")
 	assert.Equal(t, exampleUsage, addedWord.ExampleUsage, "The example usage should be correctly added")
-	err = clearTestTables(db)
-	if err != nil {
-		return
-	}
 }
 
 func TestAddWord_ExistingWord(t *testing.T) {
-	db := setupTestDB()
-	r := (&graph.Resolver{DB: db}).Mutation()
+	_, r := setupTestMutation(t)
 
 	word := "hello"
 	language := "EN"
@@ -174,15 +162,10 @@ func TestAddWord_ExistingWord(t *testing.T) {
 	assert.Equal(t, word, addedWord.Word, "The word should be the same")
 	assert.Equal(t, language, addedWord.Language, "The language should be the same")
 	assert.Equal(t, exampleUsage, addedWord.ExampleUsage, "The example usage should be the same")
-	err = clearTestTables(db)
-	if err != nil {
-		return
-	}
 }
 
 func TestAddWord_ErrorHandling(t *testing.T) {
-	db := setupTestDB()
-	r := (&graph.Resolver{DB: db}).Mutation()
+	_, r := setupTestMutation(t)
 
 	word := ""
 	language := "EN"
@@ -192,21 +175,16 @@ func TestAddWord_ErrorHandling(t *testing.T) {
 
 	require.Error(t, err, "Expected an error due to invalid word input")
 	assert.Nil(t, addedWord, "Added word should be nil due to the error")
-	err = clearTestTables(db)
-	if err != nil {
-		return
-	}
 }
 
 func TestAddWord_ConcurrentSameWord(t *testing.T) {
-	db := setupTestDB()
-	r := (&graph.Resolver{DB: db}).Mutation()
+	db, r := setupTestMutation(t)
 
 	word := "hello"
 	language := "EN"
 	exampleUsage := "A common greeting."
 
-	_, err := r.AddWord(context.Background(), word, language, exampleUsage)
+	_, _ = r.AddWord(context.Background(), word, language, exampleUsage)
 
 	done := make(chan bool)
 	for i := 0; i < 10; i++ {
@@ -222,16 +200,10 @@ func TestAddWord_ConcurrentSameWord(t *testing.T) {
 	var count int64
 	db.Find(&model.Word{}).Count(&count)
 	assert.Equal(t, int64(1), count, "Only one word inserted")
-
-	err = clearTestTables(db)
-	if err != nil {
-		return
-	}
 }
 
 func TestAddWord_ConcurrentDifferentWords(t *testing.T) {
-	db := setupTestDB()
-	r := (&graph.Resolver{DB: db}).Mutation()
+	db, r := setupTestMutation(t)
 
 	word := "hello"
 	language := "EN"
@@ -252,9 +224,60 @@ func TestAddWord_ConcurrentDifferentWords(t *testing.T) {
 	var count int64
 	db.Find(&model.Word{}).Count(&count)
 	assert.Equal(t, int64(10), count, "Only one word inserted")
+}
 
-	err := clearTestTables(db)
-	if err != nil {
-		return
-	}
+func TestDeleteWord_ExistingWord(t *testing.T) {
+	db, r := setupTestMutation(t)
+
+	word := "hello"
+	language := "EN"
+	exampleUsage := "A common greeting."
+
+	_, err := r.AddWord(context.Background(), word, language, exampleUsage)
+	require.NoError(t, err, "Expected no error while adding word initially")
+
+	deletedWord, err := r.DeleteWord(context.Background(), word, language)
+
+	require.NoError(t, err, "Expected no error while deleting word")
+	assert.NotNil(t, deletedWord, "Deleted word should not be nil")
+	assert.Equal(t, word, deletedWord.Word, "The deleted word should match")
+	assert.Equal(t, language, deletedWord.Language, "The deleted word's language should match")
+
+	var count int64
+	err = db.Model(&model.Word{}).Where("word = ? AND language = ?", word, language).Count(&count).Error
+	require.NoError(t, err, "Expected no error when counting words in database")
+	assert.Equal(t, int64(0), count, "Word should be deleted from the database")
+}
+
+func TestDeleteWord_NonExistingWord(t *testing.T) {
+	_, r := setupTestMutation(t)
+
+	word := "nonexistent"
+	language := "EN"
+
+	deletedWord, err := r.DeleteWord(context.Background(), word, language)
+
+	require.Error(t, err, "Expected error for non-existing word")
+	assert.Nil(t, deletedWord, "Deleted word should be nil for non-existing word")
+	assert.Equal(t, "word is missing in database", err.Error(), "Error message should match")
+}
+
+func TestDeleteWord_TranslationsAlsoDeleted(t *testing.T) {
+	db, r := setupTestMutation(t)
+
+	englishWord := "hello"
+	polishWord := "cześć"
+
+	_, err := r.AddTranslation(context.Background(), englishWord, polishWord)
+
+	_, err = r.DeleteWord(context.Background(), englishWord, "EN")
+
+	var count int64
+	err = db.Model(&model.Word{}).Count(&count).Error
+	require.NoError(t, err, "Expected no error when counting words in database")
+	assert.Equal(t, int64(1), count, "Only one word should be deleted from the database")
+
+	err = db.Model(&model.Translation{}).Count(&count).Error
+	require.NoError(t, err, "Expected no error when counting translations in database")
+	assert.Equal(t, int64(0), count, "Translation should be deleted from the database")
 }
